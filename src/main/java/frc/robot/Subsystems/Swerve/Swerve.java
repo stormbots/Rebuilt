9cf2e4a68036b51cabf55fb9ce6e8871db9876c9
Honeybadgrr <quinnstahlman@gmail.com>
2026-01-27 19:09:51 -0800

    Initial bringup with inputs as objects

--- src/main/java/frc/robot/Subsystems/Swerve/Swerve.java
+++ src/main/java/frc/robot/Subsystems/Swerve/Swerve.java
@@ -5,14 +5,18 @@
 package frc.robot.Subsystems.Swerve;
 
 import java.io.File;
+import java.util.Optional;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import swervelib.SwerveDrive;
 import swervelib.imu.NavXSwerve;
@@ -33,6 +37,7 @@ public class Swerve extends SubsystemBase {
   /** Creates a new SwerveSubsystem. */
   public Swerve() {
 
+    //this needs to be changed once final frame is decided
     File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"choppedbot");
     try
     {
@@ -52,28 +57,77 @@ public class Swerve extends SubsystemBase {
     
   }
 
+  static class SwerveInputs{
+    /** Positive meaning away from driver station */
+    double tx=0;
+    /** Positive meaning up from driver station */
+    double ty=0;
+    /** rotation, positive ccw*/
+    double r=0;
+    /** Zero out all inputs for this input set  */
+    public void clear(){this.tx=0;this.ty=0;this.r=0;}
+    /** Add another input to this one */
+    public SwerveInputs add(SwerveInputs other){
+      this.tx+=other.tx;
+      this.ty+=other.ty;
+      this.r+=other.r;
+      return this;
+    }
+  }
+
+  SwerveInputs driverInputs = new SwerveInputs();
+  SwerveInputs fieldInputs = new SwerveInputs();
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
     swerveDrive.updateOdometry();
 
     odometryField.setRobotPose(swerveDrive.getPose());
-  }
 
-  public Command driveAllianceManagedCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX){
-    return run(()->{
-      //when is red, the translation x and y are flipped, FIX THIS LATER WE DONT HAVE A CONSTANTS YET
-      // double allianceFlip = Constants.isRed.getAsBoolean() ? -1 : 1;
-      double allianceFlip = 1.0;
-      swerveDrive.drive(
+    var inputs = new SwerveInputs()
+    .add(driverInputs)
+    .add(fieldInputs)
+    ;
+    if(DriverStation.isDisabled())inputs.clear();
+
+    swerveDrive.drive(
         new Translation2d(
-          -allianceFlip * translationX.getAsDouble() * swerveDrive.getMaximumChassisVelocity(),
-          -allianceFlip * translationY.getAsDouble() * swerveDrive.getMaximumChassisVelocity()
+          inputs.tx * swerveDrive.getMaximumChassisVelocity(),
+          inputs.ty * swerveDrive.getMaximumChassisVelocity()
         ),
-        angularRotationX.getAsDouble() * swerveDrive.getMaximumChassisAngularVelocity(),
-        false,
+        inputs.r * swerveDrive.getMaximumChassisAngularVelocity(),
+        true,
         false 
       );
-    });
   }
+
+  public Command addDriverInputs(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX){
+    return Commands.either(
+      run(()->{
+        driverInputs.tx = 1 * translationX.getAsDouble();
+        driverInputs.ty = 1 * translationY.getAsDouble();
+        driverInputs.r = angularRotationX.getAsDouble();
+      }), 
+      run(()->{
+        driverInputs.tx = -1 * translationX.getAsDouble();
+        driverInputs.ty = -1 * translationY.getAsDouble();
+        driverInputs.r = angularRotationX.getAsDouble();
+      }),
+      ()->DriverStation.getAlliance().equals(Optional.of(Alliance.Blue))
+    )
+    .finallyDo(driverInputs::clear)
+    ;
+  }
+
+  public Command zeroGyro(){
+    return Commands.runOnce(swerveDrive::zeroGyro);
+  }
+
+  //For other subsystems/files
+  public Pose2d getSwervePose()
+  {
+    return swerveDrive.getPose();
+  }
+
 }
