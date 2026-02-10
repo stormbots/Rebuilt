47a854ab6140264bd3ded81d86938626ed44232b
Stormwork2 <tekdemo+driverstation@gmail.com>
2026-02-09 19:21:01 -0800

    climber code

---
+++ src/main/java/frc/robot/Subsystems/Climber/Climber.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.Climber;
+
+import frc.robot.Subsystems.Climber.ClimberElevator.ClimberElevator;
+import frc.robot.Subsystems.Climber.ClimberTrolley.ClimberTrolley;
+
+/** Add your docs here. */
+public class Climber {
+
+    //first stage
+    //second stage 
+
+    //Same
+        //behaviours
+        //gearing?
+        //motor rotation?
+    //different
+        // motor id
+        // Range/limits?
+        
+    ClimberElevator stage1 = new ClimberElevator(/* differences */);
+    ClimberTrolley stage2 = new ClimberTrolley(/* differences */);
+
+    public Climber(){
+    }
+    //helpful climber commands and groups to interface with things
+
+    //extend to L1 climb position
+    //goback down and climb
+
+    //maybe: Climb to L2? L3?
+}
