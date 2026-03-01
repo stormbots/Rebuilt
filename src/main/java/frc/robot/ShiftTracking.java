5d382d850b716d2786a7a8e955af0212a6661a53
fortierkids1 <fortierkids1@gmail.com>
2026-02-28 18:40:08 -0800

    added offset

--- src/main/java/frc/robot/ShiftTracking.java
+++ src/main/java/frc/robot/ShiftTracking.java
@@ -32,22 +32,23 @@ public class ShiftTracking {
     }
 
     private Boolean isScoringShift(){
+        double offset = 3.0;
         double time = Timer.getMatchTime();
         if(DriverStation.isTeleopEnabled()){
             //endgame period
-            if (time <= 30){
+            if (time <= 30+offset){
                 return true;
             }
             //transition shift
-            else if (time <= 130){
+            else if (time >= 130-offset){
                 return true;
             }
             //alliance shifts
             else if (didWeWinAuto()){
-                return (((time < 105) && (time > 80)) || ((time < 55) && (time > 30)));
+                return (((time < 105+offset) && (time > 80-offset)) || ((time < 55+offset) && (time > 30-offset)));
             }
             else{
-                return !(((time < 105) && (time > 80)) ||((time < 55) && (time > 30)));
+                return !(((time < 105+offset) && (time > 80-offset)) || ((time < 55+offset) && (time > 30-offset)));
             }
         }
         else{
