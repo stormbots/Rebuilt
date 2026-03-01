bffa9b9a1dadcf52e6d1c76d9b850d221dfe1a9d
tekdemo <tekdemo@gmail.com>
2026-02-28 18:20:35 -0800

    Windows can't unicode take 2

--- src/main/java/frc/robot/Subsystems/Lighting/LedSegment.java
+++ src/main/java/frc/robot/Subsystems/Lighting/LedSegment.java
@@ -348,25 +348,25 @@ public class LedSegment extends LedBase {
       LedMultiRange uruguay = new LedMultiRange(new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kUruguayYellow,3.75,23.0/16), new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kBlue,9,6),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kBlue,9,8),new LedRange(CustomColor.kWhite));
       LedMultiRange paraguay = new LedMultiRange(new LedRange(CustomColor.kRed),new LedRange(CustomColor.kWhite,12,5), new LedRange(CustomColor.kParaguayGreen),new LedRange(CustomColor.kWhite,60,28),new LedRange(CustomColor.kUruguayYellow),new LedRange(CustomColor.kWhite,60,33),new LedRange(CustomColor.kParaguayGreen),new LedRange(CustomColor.kWhite,12,8),new LedRange(CustomColor.kRed));
       LedMultiRange bolivia = new LedMultiRange(true, new LedRange(CustomColor.kBoliviaRed),new LedRange(CustomColor.kBoliviaYellow), new LedRange(CustomColor.kBoliviaGreen));
-      LedMultiRange perú = new LedMultiRange(true, new LedRange(CustomColor.kRed),new LedRange(CustomColor.kWhite), new LedRange(CustomColor.kRed));
+      LedMultiRange peru = new LedMultiRange(true, new LedRange(CustomColor.kRed),new LedRange(CustomColor.kWhite), new LedRange(CustomColor.kRed));
       LedMultiRange ecuador = new LedMultiRange(new LedRange(CustomColor.kEcuadorYellow),new LedRange(CustomColor.kEcuadorGreen,7.5,4.25), new LedRange(CustomColor.kEcuadorBlue),new LedRange(CustomColor.kRed,4,4));
       LedMultiRange brasil = new LedMultiRange(new LedRange(CustomColor.kBrazilGreen,5,1),new LedRange(CustomColor.kBrazilYellow), new LedRange(CustomColor.kBrazilBlue,60.0/14,37.0/14),new LedRange(CustomColor.kBrazilYellow),new LedRange(CustomColor.kBrazilGreen,5,5));
       LedMultiRange colombia = new LedMultiRange(new LedRange(CustomColor.kEcuadorYellow),new LedRange(CustomColor.kEcuadorBlue,4,3), new LedRange(CustomColor.kRed));
       LedMultiRange venezuela = new LedMultiRange(new LedRange(CustomColor.kVenezuelaYellow,3,1),new LedRange(CustomColor.kVenezuelaBlue), new LedRange(CustomColor.kWhite,30,13),new LedRange(CustomColor.kVenezuelaBlue),new LedRange(CustomColor.kWhite,30,15.5),new LedRange(CustomColor.kVenezuelaBlue),new LedRange(CustomColor.kWhite,30,18),new LedRange(CustomColor.kVenezuelaBlue),new LedRange(CustomColor.kRed,3,3));
       LedMultiRange guyana = new LedMultiRange(new LedRange(CustomColor.kGuyanaRed),new LedRange(CustomColor.kBlack,30,10), new LedRange(CustomColor.kEcuadorYellow), new LedRange(CustomColor.kWhite,30,17.5),new LedRange(CustomColor.kGuyanaGreen));
-      LedMultiRange panamá = new LedMultiRange(new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaBlue,20,3), new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaBlue,4,2),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaRed,20,13),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaRed,4,4));
-      LedMultiRange costRica = new LedMultiRange(new LedRange(CustomColor.kCostaRicaBlue),new LedRange(CustomColor.kWhite,10,2), new LedRange(CustomColor.kGuyanaRed),new LedRange(CustomColor.kWhite,10,6),new LedRange(CustomColor.kCostaRicaBlue));
+      LedMultiRange panama = new LedMultiRange(new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaBlue,20,3), new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaBlue,4,2),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaRed,20,13),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPanamaRed,4,4));
+      LedMultiRange costaRica = new LedMultiRange(new LedRange(CustomColor.kCostaRicaBlue),new LedRange(CustomColor.kWhite,10,2), new LedRange(CustomColor.kGuyanaRed),new LedRange(CustomColor.kWhite,10,6),new LedRange(CustomColor.kCostaRicaBlue));
       LedMultiRange nicaragua = new LedMultiRange(new LedRange(CustomColor.kNicaraguaBlue),new LedRange(CustomColor.kWhite,7.5,3.5), new LedRange(CustomColor.kNicaraguaGreen),new LedRange(CustomColor.kWhite,7.5,5),new LedRange(CustomColor.kNicaraguaBlue));
       LedMultiRange honduras = new LedMultiRange(new LedRange(CustomColor.kHondurasBlue),new LedRange(CustomColor.kWhite,15,6), new LedRange(CustomColor.kHondurasBlue),new LedRange(CustomColor.kWhite,20,29.0/3),new LedRange(CustomColor.kHondurasBlue),new LedRange(CustomColor.kWhite,20,34.0/3),new LedRange(CustomColor.kHondurasBlue),new LedRange(CustomColor.kWhite,15,10),new LedRange(CustomColor.kHondurasBlue));
       LedMultiRange elSalvador = new LedMultiRange(new LedRange(CustomColor.kElSalvadorBlue),new LedRange(CustomColor.kWhite,10,26.0/6), new LedRange(CustomColor.kElSalvadorGreen),new LedRange(CustomColor.kWhite,10,40.0/6),new LedRange(CustomColor.kElSalvadorBlue));
       LedMultiRange guatemala = new LedMultiRange(new LedRange(CustomColor.kGuatemalaBlue),new LedRange(CustomColor.kWhite,12,5), new LedRange(CustomColor.kGuatemalaGreen),new LedRange(CustomColor.kWhite,12,8),new LedRange(CustomColor.kGuatemalaBlue));
-      LedMultiRange méxico = new LedMultiRange(new LedRange(CustomColor.kMexicoGreen),new LedRange(CustomColor.kWhite,60.0/7,27.0/7), new LedRange(CustomColor.kMexicoBrown),new LedRange(CustomColor.kWhite,60.0/7,40.0/7), new LedRange(CustomColor.kMexicoRed));
+      LedMultiRange mexico = new LedMultiRange(new LedRange(CustomColor.kMexicoGreen),new LedRange(CustomColor.kWhite,60.0/7,27.0/7), new LedRange(CustomColor.kMexicoBrown),new LedRange(CustomColor.kWhite,60.0/7,40.0/7), new LedRange(CustomColor.kMexicoRed));
       LedMultiRange cuba = new LedMultiRange(new LedRange(CustomColor.kCubaBlue),new LedRange(CustomColor.kWhite,7.5,2), new LedRange(CustomColor.kCubaBlue),new LedRange(CustomColor.kBoliviaRed,10,29.0/6),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kBoliviaRed,10,37.0/6), new LedRange(CustomColor.kCubaBlue),new LedRange(CustomColor.kWhite,7.5,52.0/8) ,new LedRange(CustomColor.kCubaBlue));
-      LedMultiRange repúblicaDominicana = new LedMultiRange(new LedRange(CustomColor.kRepublicaDominicanaBlue),new LedRange(CustomColor.kWhite,15,4), new LedRange(CustomColor.kRepublicaDominicanaRed),new LedRange(CustomColor.kRepublicaDominicanaGreen,15,8), new LedRange(CustomColor.kRepublicaDominicanaBlue), new LedRange(CustomColor.kWhite,15,12),new LedRange(CustomColor.kRepublicaDominicanaRed));
+      LedMultiRange repeblicaDominicana = new LedMultiRange(new LedRange(CustomColor.kRepublicaDominicanaBlue),new LedRange(CustomColor.kWhite,15,4), new LedRange(CustomColor.kRepublicaDominicanaRed),new LedRange(CustomColor.kRepublicaDominicanaGreen,15,8), new LedRange(CustomColor.kRepublicaDominicanaBlue), new LedRange(CustomColor.kWhite,15,12),new LedRange(CustomColor.kRepublicaDominicanaRed));
       LedMultiRange jamaica = new LedMultiRange(true, new LedRange(CustomColor.kJamaicaGreen),new LedRange(CustomColor.kEcuadorYellow), new LedRange(CustomColor.kBlack),new LedRange(CustomColor.kEcuadorYellow),new LedRange(CustomColor.kJamaicaGreen));
-      LedMultiRange haití = new LedMultiRange(new LedRange(CustomColor.kHaitiBlue),new LedRange(CustomColor.kWhite,7.5,4.25), new LedRange(CustomColor.kHaitiRed));
+      LedMultiRange haiti = new LedMultiRange(new LedRange(CustomColor.kHaitiBlue),new LedRange(CustomColor.kWhite,7.5,4.25), new LedRange(CustomColor.kHaitiRed));
       LedMultiRange unitedStates = new LedMultiRange(new LedRange(CustomColor.kUnitedStatesBlue),new LedRange(CustomColor.kWhite,30,4), new LedRange(CustomColor.kUnitedStatesBlue),new LedRange(CustomColor.kWhite,30,8),new LedRange(CustomColor.kUnitedStatesBlue),new LedRange(CustomColor.kWhite,30,12),new LedRange(CustomColor.kUnitedStatesBlue),new LedRange(CustomColor.kWhite,12,7),new LedRange(CustomColor.kUnitedStatesRed),new LedRange(CustomColor.kWhite,12,9),new LedRange(CustomColor.kUnitedStatesRed),new LedRange(CustomColor.kWhite,12,11),new LedRange(CustomColor.kUnitedStatesRed));
-      LedMultiRange canadá = new LedMultiRange(new LedRange(CustomColor.kCanadaRed),new LedRange(CustomColor.kWhite,12,5), new LedRange(CustomColor.kCanadaRed),new LedRange(CustomColor.kWhite,12,8),new LedRange(CustomColor.kCanadaRed));
+      LedMultiRange canada = new LedMultiRange(new LedRange(CustomColor.kCanadaRed),new LedRange(CustomColor.kWhite,12,5), new LedRange(CustomColor.kCanadaRed),new LedRange(CustomColor.kWhite,12,8),new LedRange(CustomColor.kCanadaRed));
       LedMultiRange puertoRico = new LedMultiRange(new LedRange(CustomColor.kPuertoRicoRed),new LedRange(CustomColor.kWhite,7.5,2), new LedRange(CustomColor.kPuertoRicoRed),new LedRange(CustomColor.kPuertoRicoBlue,10,29.0/6),new LedRange(CustomColor.kWhite),new LedRange(CustomColor.kPuertoRicoBlue,10,37.0/6), new LedRange(CustomColor.kPuertoRicoRed),new LedRange(CustomColor.kWhite,7.5,52.0/8) ,new LedRange(CustomColor.kPuertoRicoRed));
       
       return new SequentialCommandGroup(stripes(chile)
@@ -379,7 +379,7 @@ public class LedSegment extends LedBase {
       .andThen(new WaitCommand(duration))
       .andThen(stripes(bolivia))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(perú))
+      .andThen(stripes(peru))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(ecuador))
       .andThen(new WaitCommand(duration))
@@ -391,9 +391,9 @@ public class LedSegment extends LedBase {
       .andThen(new WaitCommand(duration))
       .andThen(stripes(guyana))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(panamá))
+      .andThen(stripes(panama))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(costRica))
+      .andThen(stripes(costaRica))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(nicaragua))
       .andThen(new WaitCommand(duration))
@@ -403,56 +403,21 @@ public class LedSegment extends LedBase {
       .andThen(new WaitCommand(duration))
       .andThen(stripes(guatemala))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(méxico))
+      .andThen(stripes(mexico))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(cuba))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(repúblicaDominicana))
+      .andThen(stripes(repeblicaDominicana))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(jamaica))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(haití))
+      .andThen(stripes(haiti))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(unitedStates))
       .andThen(new WaitCommand(duration))
-      .andThen(stripes(canadá))
+      .andThen(stripes(canada))
       .andThen(new WaitCommand(duration))
       .andThen(stripes(puertoRico))
       .andThen(new WaitCommand(duration)));
     }
-
-
-
-    // seguimos aquí
-    // Chile {"seg":{"i":[0,8,"0032A0",8,12,"FFFFFF",12,20,"0032A0",20,40,"FFFFFF",40,60,"DA291C"]}}
-    // Argentina {"seg":{"i":[0,20,"6CACE4",20,25,"FFFFFF",25,35,"FFB81C",35,40,"FFFFFF",40,60,"6CACE4"]}}
-    // Uruguay {"seg":{"i":[0,7,"FFFFFF",7,23,"FFCD00",23,30,"FFFFFF",30,37,"0000FF",37,44,"FFFFFF",44,51,"0000FF",51,60,"FFFFFF"]}} 9 stripes
-    // Paraguay {"seg":{"i":[0,20,"FF0000",20,25,"FFFFFF",25,27,"1ccc00",27,28,"FFFFFF",28,32,"FFCD00",32,33,"FFFFFF",33,35,"1ccc00",35,40,"FFFFFF",40,60,"0000FF"]}}
-    // Bolivia {"seg":{"i":[0,20,"DA291C",20,26,"F8E600",26,29,"DA291C",29,31,"F8E600",31,34,"DA291C",34,40,"F8E600",40,60,"007A33"]}}
-    //        {"seg":{"i":[0,20,"DA291C",20,40,"F8E600",40,60,"007A33"]}}
-    // Perú {"seg":{"i":[0,20,"FF0000",20,25,"FFFFFF",25,27,"1ccc00",27,28,"FFFFFF",28,32,"FF0000",32,33,"FFFFFF",33,35,"1ccc00",35,40,"FFFFFF",40,60,"FF0000"]}}
-    //     {"seg":{"i":[0,20,"FF0000",20,40,"FFFFFF",40,60,"FF0000"]}}
-    // Ecuador {"seg":{"i":[0,26,"FFD100",26,34,"0e9c1f",34,45,"0072CE",45,60,"FF0000"]}}
-    // Brasil {"seg":{"i":[0,12,"009739",12,23,"FEDD00",23,37,"012169",37,48,"FEDD00",48,60,"009739"]}}
-    // Colombia {"seg":{"i":[0,30,"FFD100",30,45,"0072CE",45,60,"FF0000"]}}
-    // Venezuela {"seg":{"i":[0,20,"FCE300",20,24,"003DA5",24,26,"FFFFFF",26,29,"003DA5",29,31,"FFFFFF",31,34,"003DA5",34,36,"FFFFFF",36,40,"003DA5",40,60,"FF0000"]}}
-    //            check star size
-    // Guyana {"seg":{"i":[0,18,"Ef3340",18,20,"000000",20,33,"FFD100",33,35,"FFFFFF",35,60,"009739"]}}
-    // Panamá {"seg":{"i":[0,6,"FFFFFF",6,9,"0d44a8",9,15,"FFFFFF",15,30,"0d44a8",30,36,"FFFFFF",36,39,"DA121A",39,45,"FFFFFF",45,60,"DA121A"]}}
-    // Costa Rica {"seg":{"i":[0,10,"00205B",10,20,"FFFFFF",20,40,"Ef3340",40,50,"FFFFFF",50,60,"00205B"]}}
-    // Nicaragua {"seg":{"i":[0,20,"0067c6",20,28,"FFFFFF",28,32,"02de95",32,40,"FFFFFF",40,60,"0067c6"]}}
-    // Honduras {"seg":{"i":[0,20,"0091d4",20,24,"FFFFFF",24,26,"0091d4",26,29,"FFFFFF",29,31,"0091d4",31,34,"FFFFFF",34,36,"0091d4",36,40,"FFFFFF",40,60,"0091d4"]}}
-    // El Salvador {"seg":{"i":[0,20,"0047AB",20,26,"FFFFFF",26,34,"009900",34,40,"FFFFFF",40,60,"0047AB"]}}
-    // Guatemala {"seg":{"i":[0,20,"4997D0",20,25,"FFFFFF",25,35,"269401",35,40,"FFFFFF",40,60,"4997D0"]}}
-    // México {"seg":{"i":[0,20,"006341",20,27,"FFFFFF",27,33,"753100",33,40,"FFFFFF",40,60,"c8102E"]}}
-    // Cuba {"seg":{"i":[0,8,"004B87",8,16,"FFFFFF",16,23,"004B87",23,29,"DA291C",29,31,á"FFFFFF",31,37,"DA291C",37,44,"004B87",44,52,"FFFFFF",52,60,"004B87"]}}
-    // República Dominicana {"seg":{"i":[0,12,"002D62",12,16,"FFFFFF",16,28,"CE1126",28,32,"006300",32,44,"002D62",44,48,"FFFFFF",48,60,"CE1126"]}}
-    // Jamaica {"seg":{"i":[0,12,"009B3A",12,24,"FED100",24,36,"000000",36,48,"FED100",48,60,"009B3A"]}}
-    // Haití {"seg":{"i":[0,26,"00209F",26,34,"FFFFFF",34,60,"D21034"]}}
-    // Las Antillas
-    // United States {"seg":{"i":[0,6,"0A3161",6,8,"FFFFFF",8,14,"0A3161",14,16,"FFFFFF",16,22,"0A3161",22,24,"FFFFFF",24,30,"0A3161",30,35,"FFFFFF",35,40,"B31942",40,45,"FFFFFF",45,50,"B31942",50,55,"FFFFFF",55,60,"B31942"]}}
-    // Canadá {"seg":{"i":[0,20,"D80621",20,25,"FFFFFF",25,35,"D80621",35,40,"FFFFFF",40,60,"D80621"]}}
-    // Puerto Rico {"seg":{"i":[0,8,"Ce0000",8,16,"FFFFFF",16,23,"Ce0000",23,29,"33adff",29,31,"FFFFFF",31,37,"33adff",37,44,"Ce0000",44,52,"FFFFFF",52,60,"Ce0000"]}}
-
-
   }
