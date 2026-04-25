4f0baa5dff9afceb18f02c705b9f7337c16380fa
fortierkids1 <fortierkids1@gmail.com>
2026-04-24 18:27:08 -0700

    Added controller rumble for shift change

---
+++ src/main/java/frc/robot/Subsystems/DriverFeedback/DriverFeedback.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.Subsystems.DriverFeedback;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+/** Add your docs here. */
+public class DriverFeedback {
+    CommandXboxController driver;
+    CommandXboxController operator;
+
+    public DriverFeedback(CommandXboxController driver, CommandXboxController operator){
+        this.driver = driver;
+        this.operator = operator;
+    }
+
+    public Command shiftStart(){
+        return Commands.parallel(
+            new RumbleBoth(driver, 1.0, 0.5),
+            new RumbleBoth(operator, 1.0, 0.5)
+        );
+    }
+
+    public Command shiftEnd(){
+        return Commands.parallel(
+            new PulseBoth(driver, 1.0, 0.5, 0.5),
+            new PulseBoth(operator, 1.0, 0.5, 0.5)
+        );
+    }
+}
